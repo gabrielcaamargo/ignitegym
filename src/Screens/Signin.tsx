104d5bf582ee3430f14d5e05b160d1d640c801f8
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-02-26 19:55:38 -0300

    feat: create input component

--- src/Screens/Signin.tsx
+++ src/Screens/Signin.tsx
@@ -2,10 +2,11 @@ import { VStack, Image, Text, Center, Heading } from 'native-base'
 
 import BackgroundImage from '@assets/background.png'
 import SvgLogo from '@assets/logo.svg'
+import { Input } from '@/components/Input';
 
 export function Signin() {
   return (
-    <VStack flex={1} bg='gray.700'>
+    <VStack flex={1} bg='gray.700' px={8}>
       <Image 
         source={BackgroundImage}
         alt='People working out'
@@ -22,6 +23,16 @@ export function Signin() {
         <Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
           Acesse sua conta
         </Heading>
+
+        <Input 
+          placeholder='Email'
+          keyboardType='email-address'
+          autoCapitalize='none'
+        />
+        <Input 
+          placeholder='Senha'
+          secureTextEntry
+        />
       </Center>
     </VStack>
   );
