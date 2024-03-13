eaea55b13b1c38408a8a82f6f7df560862362d07
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-03-13 13:36:32 -0300

    feat: create logout button on home header component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,16 +1,25 @@
-import { Heading, Text, HStack, VStack } from "native-base";
+import { Heading, Text, HStack, VStack, Icon } from "native-base";
 import { UserPhoto } from "./UserPhoto";
+
+import { MaterialIcons } from '@expo/vector-icons'
+import { TouchableOpacity } from "react-native";
 
 export function HomeHeader() {
   return (
-    <HStack bg='gray.800' pt={16} pb={6} px={8} alignItems='center'>
+    <HStack 
+      bg='gray.800' 
+      pt={16} 
+      pb={6} 
+      px={8} 
+      alignItems='center'
+    >
       <UserPhoto 
         source={{ uri: 'https://github.com/gabrielcaamargo.png' }}
         size={16}
         alt='Imagem do usuário'
         mr={4}
       />
-      <VStack>
+      <VStack flex={1}>
         <Text color='gray.100' fontSize='md'>
           Olá,
         </Text>
@@ -19,6 +28,15 @@ export function HomeHeader() {
           Gabriel
         </Heading>
       </VStack>
+
+      <TouchableOpacity>
+        <Icon 
+          as={MaterialIcons}
+          name='logout'
+          color='gray.400'
+          size={8}
+        />
+      </TouchableOpacity>
     </HStack>
   )
 }
